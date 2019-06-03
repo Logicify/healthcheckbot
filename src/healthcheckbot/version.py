1b8d829257881c0d0b9304a087bd96ee5b8cf325
Dmitry Berezovsky <dmitry.berezovsky@logicify.com>
2019-06-03 18:45:11 +0300

    Version bump

--- src/healthcheckbot/version.py
+++ src/healthcheckbot/version.py
@@ -14,4 +14,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "0.1.8"
+version = "0.1.9"
